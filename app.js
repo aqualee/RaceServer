ea21ea2e971165adffdd636d9b9e933dfe095f84
aqualee <aqualeepay@163.com>
2019-01-16 17:25:22 +0800

    +++

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var RedisStore = require('connect-redis')(session);
 var bodyParser = require('body-parser');
 const login = require('./wxLogin/login');
 const WXBizDataCrypt = require('./wxLogin/WXBizDataCrypt');
+var redis = require('redis');
+var mysql = require('mysql');
+
 
 const co = require('co');
 const constants= require('./constants');
@@ -13,13 +16,29 @@ const appId = 'wx70940596da02587d';
 const appSecret= "3a443af56d1d669f392a2d086e6cf0a7";
 
 var app = express()
+var redisClient= redis.createClient(
+    {
+        host:'127.0.0.1',
+        port:6379,
+        ttl:24*60*60
+    }
+);
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : '127.0.0.1',
+    port            : 3306,
+    user            : 'raceUser',
+    password        : 'a123456',
+    database        : 'race'
+  });
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(session({
     store: new RedisStore({
-        host:'127.0.0.1',
-        port:6379,
-        ttl:24*60*60
+        client :redisClient
     }),
     secret: 'keyboard cat',
     resave: false,
@@ -62,6 +81,10 @@ var loginWX2 = co.wrap(function*  loginWX(code,callback){
     callback(null,obj);
 });
 
+
+var getDataFromDB = function(){
+
+}
 
 app.get('/user/login',function (req,res){
     //var msg = req.body;
@@ -133,10 +156,100 @@ app.post('/user/weakLogin',function (req,res){
 });
 
 
+//取玩家数据
+app.post('/user/getData',function(req,res){
+    if(req.session && req.session.openId){
+        var sql='select * from user where openId = ?';
+        var args=[req.session.openId];
+        pool.query(sql,args,function(err,res,field){
+            if(err){
+                //db 出错
+                res.send(getParam(constants.CLIENT_STATUS_ERROR));
+                return;
+            }
+            if(res && res.length > 0){
+                var rs = res[0];
+                req.session.isDBCreate = true;
+                res.send(getParam(constants.CLIENT_STATUS_OK,rs.data));
+            }else{
+                //没有数据
+                res.send(getParam(constants.CLIENT_STATUS_OK));
+            }
+        });
+    }else{
+        //没有session ，需要重新登入
+        res.send(getParam(constants.CLIENT_STATUS_SESSION_EXPIRE));
+    }
+});
+
+//保存玩家数据
+app.post('/user/saveData',function(req,res){
+    var jdata = req.body.data;
+    if(req.session && req.session.openId){
+        if(req.session.isDBCreate){
+            //update
+            var sql='update user set data = ? where id= ?';
+            var args = [jdata,req.session.openId];
+            pool.query(sql,args,function(err,res){
+                if(err){
+                    //db 出错
+                    res.send(getParam(constants.CLIENT_STATUS_ERROR));
+                    return;
+                }                
+                res.send(getParam(constants.CLIENT_STATUS_OK));
+            });
+        }else{
+            //insert
+
+            var sql = 'insert into user (openId,data,userInfo,lastLoginTime values (?,?,?,?))';
+            var loginTime = Date.now();
+            var userInfo = req.session.isAuth?req.session.userInfo:null;
+            var args = [req.session.openId, jdata, userInfo , loginTime];
+            pool.query(sql,args,function(err,res){
+                if(err){
+                    //db 出错
+                    res.send(getParam(constants.CLIENT_STATUS_ERROR));
+                    return;
+                }
+                req.session.isDBCreate = true;
+                res.send(getParam(constants.CLIENT_STATUS_OK));
+            });
+        }
+    }else{
+        //没有session ，需要重新登入
+        res.send(getParam(constants.CLIENT_STATUS_SESSION_EXPIRE));
+    }
+});
+
+//看其他玩家数据
+app.post('/user/saveData',function(req,res){
+    var openId = req.body.openId;
+
+
+
+
+});
+
+
  
-app.listen(port,()=>{console.log("server listening on port "+port)})
+const server =app.listen(port,()=>{console.log("server listening on port "+port)})
+
+
 
 
 process.on('uncaughtException', function(err) {
 	console.error(' Caught exception: ' + err.stack);
 });
+
+
+process.on('SIGINT', () => {
+    redisClient.quit();
+    server.close(() => {
+        pool.end(
+            (err)=>{
+                console.log('Process terminated');
+                process.exit(code=0);
+            }
+        )
+    })
+})
--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-    CLIENT_STATUS_ERROR: 0,
-    CLIENT_STATUS_OK: 1,
+    CLIENT_STATUS_ERROR: 0, //错误
+    CLIENT_STATUS_OK: 1, //正常
+    CLIENT_STATUS_SESSION_EXPIRE:500 //session 失效需要重新login
 
 };
